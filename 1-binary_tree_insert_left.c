93b90405ab41c2ded206f330c4ab444c6230bf29
johnyREx <johnkwofie99@icloud.com>
2023-10-31 02:31:53 -0700

    trailing white space fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
-	
+
 	if (new_node == NULL)
 		return (NULL);
 
