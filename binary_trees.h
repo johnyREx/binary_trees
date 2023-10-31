25a9c738db00fa4ecf8b9f9c4f1c62d34abc65bb
johnyREx <johnkwofie99@icloud.com>
2023-10-30 17:55:22 -0700

    binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -12,11 +12,12 @@ struct binary_tree_s
 
 /* typedefs for binary tree types */
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary-tree_s bst_t;
+typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
