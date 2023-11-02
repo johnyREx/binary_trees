99781877213ad851e37e266cca1ac3fe9fc53535
johnyREx <johnkwofie99@icloud.com>
2023-11-02 14:12:09 -0700

    tree is full error fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,8 +16,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) &&
-			       	binary_tree_is_full(tree->right));
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
 	return (0);
 }
