3947c4aa75403155be96d5f72b743392046e8d19
johnyREx <johnkwofie99@icloud.com>
2023-11-02 15:33:34 -0700

    binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,10 +1,11 @@
+#include <stdlib.h>
 #include <stddef.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measure the height 
- * @tree: Pointer to the node
- * Return: Height of the tree, 0 if tree is nULL
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Height of the tree
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -14,30 +15,52 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	 left_height = binary_tree_height(tree->left);
-	 right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	 return (1 + (left_height > right_height ? left_height : right_height));
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
- * binary_tree_is_perfect - Checks is binary tree is perfect
- * @tree: Pointer to the root node
- * Return: 1 if tree is perfect, 0 otherwise
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree
+ * Return: 1 if the tree is perfect, 0 otherwise
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height, size, expected_size;
-
+	size_t height, i;
+	
 	if (tree == NULL)
 		return (0);
 
 	height = binary_tree_height(tree);
-	size = binary_tree_size(tree);
+
+	for (i = 0; i < height; i++)
+	{
+		if (binary_tree_is_perfect_level(tree, i) == 0)
+			return (0);
+	}
 
 
-	expected_size = (1 << height) - 1;
+	return (1);
+}
 
-	return (size == expected_size);
+/**
+ * binary_tree_is_perfect_level - Checks if a binary tree level is perfect
+ * @tree: Pointer to the root node of the tree
+ * @level: Level to check
+ * Return: 1 if the level is perfect, 0 otherwise
+ */
+
+int binary_tree_is_perfect_level(const binary_tree_t *tree, size_t level)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (level == 0)
+		return (1);
+
+	return (binary_tree_is_perfect_level(tree->left, level - 1) &&
+			binary_tree_is_perfect_level(tree->right, level - 1));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -36,7 +36,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+int binary_tree_is_perfect_level(const binary_tree_t *tree, size_t level);
 /* Functions to print the bianry tree */
 void binary_tree_print(const binary_tree_t *tree);
 
